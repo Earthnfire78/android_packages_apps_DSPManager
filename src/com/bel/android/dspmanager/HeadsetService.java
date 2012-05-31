852e0fa120e42d40e856f105b89428929868408e
Earthnfire78 <earthnfire78@gmail.com>
2012-05-30 19:03:17 -0600

    Removed Virtualizer Room Effects

--- src/com/bel/android/dspmanager/HeadsetService.java
+++ src/com/bel/android/dspmanager/HeadsetService.java
@@ -16,7 +16,6 @@ import android.media.AudioManager;
 import android.media.audiofx.AudioEffect;
 import android.media.audiofx.BassBoost;
 import android.media.audiofx.Equalizer;
-import android.media.audiofx.Virtualizer;
 import android.os.IBinder;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
@@ -52,7 +51,6 @@ public class HeadsetService extends Service {
 
     private AudioManager mAudioManager;
 	private Equalizer equalizer;
-	private Virtualizer virtualizer;
 	private BassBoost bassBoost;
 
 	protected boolean useHeadphone;
@@ -136,7 +134,6 @@ public class HeadsetService extends Service {
 		Log.i(TAG, "Starting service.");
 
 		equalizer = new Equalizer(0, 0);
-		virtualizer = new Virtualizer(0, 0);
 		bassBoost = new BassBoost(0, 0);
 
 		startForeground(DSPManager.NOTIFY_FOREGROUND_ID, new Notification());
@@ -229,14 +226,10 @@ public class HeadsetService extends Service {
 			}
 			catch (Exception e) {
 				throw new RuntimeException(e);
-			}
-		}
-
-		{
-			virtualizer.setEnabled(preferences.getBoolean("dsp.headphone.enable", false));
-			if (virtualizer.getStrengthSupported()) {
-				String strength = preferences.getString("dsp.headphone.mode", "0");
-				virtualizer.setStrength(Short.valueOf(strength));
+			
+			/* Here is were virtualizer effects would go, I removed it as virtualizing effects
+                         * are shit anyways.   Plus these kinds of effects can damage the cheap headsets and
+                         * headphones we all have. */
 			}
 		}
 	}	
